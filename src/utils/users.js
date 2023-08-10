518c3bcc7f56d4270aa09c05c2e060d2ff631355
Hernan Parino <you@example.com>
2023-08-10 00:21:44 -0300

    antes de empesar a trabajar con secualize

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,3 +1,4 @@
 
 module.exports = [{email: 'user@mail.com', password: 'abcd1234'},
-{email: 'parinohernan@gmail.com', password: 'qwerty1234'}];
+{email: 'parinohernan@gmail.com', password: 'qwerty1234'},
+{email: 'mail@mail.com', password: '1234'}];
